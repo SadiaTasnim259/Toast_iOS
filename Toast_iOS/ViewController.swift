bd7545c582a5b8098781924123bc5ae246cd31b8
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-08-03 10:26:05 +0600

    Toast code for iOS Projects

--- Toast_iOS/ViewController.swift
+++ Toast_iOS/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func continueButtonPressed(_ sender: UIButton) {
+        self.showToast(message: "Please Enter Your Name", bottomPadding: 120)
+    }
+    
 }
 
